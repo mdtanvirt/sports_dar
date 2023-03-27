9f246a81e4580b0ee9d320bb77da0bdcbd34d22e
tanvir <mdtanvirt@gmail.com>
2023-03-27 13:17:59 +0600

    initiate project with nav bar

--- app.py
+++ app.py
@@ -2,4 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('This is Tanvir')
+st.set_page_config(layout="wide")
+
+# Hide streamlit default menu and footer from the template
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden}
+    footer {visibility: hidden}
+    header {visibility: hidden}
+    </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+#st.title('This is Tanvir')
+nav = st.sidebar.radio("Navigation", ["Home", "Prediction", "Conribute"])
