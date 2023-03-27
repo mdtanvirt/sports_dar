d0bdfee0207254f6b022c6aa1d64d4a0fde79860
tanvir <mdtanvirt@gmail.com>
2023-03-27 15:18:45 +0600

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_option_menu import option_menu
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 # Hide streamlit default menu and footer from the template
 hide_st_style = """
@@ -12,8 +13,11 @@ hide_st_style = """
     header {visibility: hidden}
     </style>
 """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-st.header('Sports Analysis')
+#st.markdown(hide_st_style, unsafe_allow_html=True)
+#st.header('Sports Analysis')
 
 #st.title('This is Tanvir')
-nav = st.sidebar.radio("Navigation", ["Home", "Prediction", "Conribute"])
+#nav = st.sidebar.radio("Navigation", ["Home", "Prediction", "Conribute"])
+with st.sidebar:
+    sselected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=0)
